0a75c4511124d1396cb0fe8d10c2c8228ee4d661
Dima Doroshev <github@doroshev.com>
2021-04-23 19:58:21 +0300

    fin

--- multifetcher/main.py
+++ multifetcher/main.py
@@ -1,27 +1,35 @@
 import json
 import asyncio
-from aiohttp import web, ClientSession
+from aiohttp import web, client
 
-async def make_request(params, *, session, response):
-    async with session.get(params['url']) as resp:
+
+async def make_request(params, *, response):
+    async with client.request(
+        params["method"],
+        params["url"],
+        headers=params.get("headers"),
+        data=params.get("data"),
+        json=params.get("json"),
+    ) as resp:
         resp_data = await resp.text()
-        result = {'url': params['url'], 'response': resp_data}
-        await response.write((json.dumps(result) + '\n').encode())
+        result = {"url": params["url"], "response": resp_data}
+        await response.write((json.dumps(result) + "\n").encode())
+
 
 async def requester(request):
     response = web.StreamResponse()
     await response.prepare(request)
     tasks = []
-    async with ClientSession() as session:
-        for params in await request.json():
-            task = asyncio.ensure_future(make_request(params, session=session, response=response))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+    for params in await request.json():
+        task = asyncio.ensure_future(make_request(params, response=response))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
     await response.write_eof()
     return response
 
-app = web.Application()
-app.add_routes([web.post('/', requester)])
 
-if __name__ == '__main__':
+app = web.Application()
+app.add_routes([web.post("/", requester)])
+
+if __name__ == "__main__":
     web.run_app(app)
--- test.py
+++ test.py
@@ -4,13 +4,86 @@ import json
 TEST_PARAMS = [
     {"method": "GET", "url": "https://google.com"},
     {"method": "GET", "headers": {"Cookie": "foo=bar"}, "url": "https://yandex.ru"},
-    {"method": "GET", "headers": {"Foo": "Bar"}, "url": "https://httpbin.org/json"}
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
+    {
+        "method": "POST",
+        "headers": {"Foo": "Bar"},
+        "url": "https://httpbin.org/post",
+        "json": {"Boom": "Wow"},
+    },
 ]
 
-def run():
-    resp = requests.post('http://localhost:8000/', json=TEST_PARAMS, stream=True)
-    for line in resp.iter_lines():
-        print('###', json.loads(line.decode()))
 
-if __name__ == '__main__':
+def run():
+    resp = requests.post("http://localhost:8000/", json=TEST_PARAMS, stream=True)
+    for line in resp.iter_lines():
+        print("###", json.loads(line.decode()))
+
+
+if __name__ == "__main__":
     run()
